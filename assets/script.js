1415d874571365c1d2425b76d7c980165e374721
16 Caracteres <camille@16caracteres.com>
2024-03-27 16:16:00 +0100

    Etape 2 : addEventListener

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,14 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrowLeft = document.querySelector(".arrow_left")
+let arrowRight = document.querySelector(".arrow_right")
+
+arrowLeft.addEventListener("click", () => {
+	console.log("Je clique sur la flèche gauche")
+})
+
+arrowRight.addEventListener("click", () => {
+	console.log("Je clique sur la flèche droite")
+})
